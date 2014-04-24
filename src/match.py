18ce96dd0f226ff11bea42d11752ef212fe83917
YuviPanda <yuvipanda@gmail.com>
2014-04-24 20:08:04 +0530

    Move wikidata querying into a class
    Also output JSON of missing page ids

--- src/match.py
+++ src/match.py
@@ -1,4 +1,6 @@
 #!/bin/bash
+import json
+
 import pandora_client as pandora
 from mwapi import MWApi
 
@@ -8,9 +10,57 @@ class PandoItem(object):
         self.id = id
         self.enwiki_title = enwiki_title
 
+class PandoItemList(object):
+    def __init__(self, items):
+        self.items = items
+        self.items_missing_wikidata = []
+
+    def get_one_item(self, filter):
+        for item in self.items:
+            if filter(item):
+                return item
+        return None
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def get_wikidata_items(self):
+        enwiki_titles = []
+        for pi in self.items:
+            enwiki_titles.append(pi.enwiki_title)
+        resp = mwapi.get(
+            action="wbgetentities",
+            sites="enwiki",
+            titles="|".join(enwiki_titles),
+            props="info|claims"
+        )
+        itemsJSON = resp["entities"].values()
+
+        for item in itemsJSON:
+            title = item["title"]
+            if "id" in item:
+                id = item["id"]
+            else:
+                # Item has been deleted
+                id = None
+                self.items_missing_wikidata.append(
+                    self.get_one_item(lambda pi: pi.enwiki_title == item["title"])
+                )
+            claims = {}
+            if "claims" in item:
+                for property, claimJSON in item["claims"].iteritems():
+                    datavalue = claimJSON[0]["mainsnak"]["datavalue"]
+                    if "type" in datavalue and datavalue["type"] == "wikibase-entityid":
+                        value = "Q" + unicode(datavalue["value"]["numeric-id"])
+                    else:
+                        value = datavalue["value"]
+                    claims[property] = value
+
+            yield WikiDataItem(id, title, claims)
+
 
 class PandoQuery(object):
-    def __init__(self, conditions, operator="&", items_per_page=100):
+    def __init__(self, conditions, operator="&", items_per_page=50):
         self._query = {
             'conditions': conditions,
             'operator': operator
@@ -35,43 +85,12 @@ class PandoQuery(object):
 
         self._start_item_number += self._items_per_page
 
-        return items
+        return PandoItemList(items)
+
 
 missing_titles = []
 
 mwapi = MWApi("https://www.wikidata.org")
-def claims_for_pando_items(pando_items, wiki="enwiki"):
-    enwiki_titles = []
-    for pi in pando_items:
-        enwiki_titles.append(pi.enwiki_title)
-    resp = mwapi.get(
-        action="wbgetentities",
-        sites=wiki,
-        titles="|".join(enwiki_titles),
-        props="info|claims"
-    )
-    items = resp["entities"].values()
-
-    for item in items:
-        title = item["title"]
-        if "id" in item:
-            id = item["id"]
-        else:
-            # Item has been deleted
-            id = None
-            missing_titles.append(item["title"])
-        claims = {}
-        if "claims" in item:
-            for property, claimJSON in item["claims"].iteritems():
-                datavalue = claimJSON[0]["mainsnak"]["datavalue"]
-                if "type" in datavalue and datavalue["type"] == "wikibase-entityid":
-                    value = "Q" + unicode(datavalue["value"]["numeric-id"])
-                else:
-                    value = datavalue["value"]
-                claims[property] = value
-
-        yield WikiDataItem(id, title, claims)
-
 
 class WikiDataItem(object):
     def __init__(self, id, title, claims):
@@ -85,11 +104,12 @@ query = PandoQuery([
     {"key": "list", "value": "j:Wikipedia"}
 ])
 wds = []
+missing_ids = []
 
 pando_items = query.get_next_page()
-while len(pando_items):
-    claims_for_pando_items(pando_items)
-    wds.extend(claims_for_pando_items(pando_items))
+while not pando_items.is_empty():
+    wds.extend(pando_items.get_wikidata_items())
+    missing_ids.extend([pi.id for pi in pando_items.items_missing_wikidata])
     print "Done for %d, missing %d" % (len(wds), len(missing_titles))
     pando_items = query.get_next_page()
 
@@ -98,4 +118,4 @@ for wd in wds:
         properties_count[prop] = properties_count.get(prop, 0) + 1
 
 print properties_count
-print missing_titles
+open("missing_ids", "w").write(json.dumps(missing_ids))
